afe213faa588e364b4b9b45e67c4c9f65f9aa969
“Ravi <patilravi400@gmail.com>
2022-09-18 16:56:01 +0530

    first commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Compo.css'
+import { Link } from "react-router-dom";
+function Home() {
+    return (
+      <div>
+        <center>
+          <h1 className="Home">Hello</h1>
+          
+        </center>
+      </div>
+    );
+  }
+
+  export default Home;
